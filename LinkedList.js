a54cc9746135c440e82c8fe6c05a1f503fb3544e
DizzyMoon <mikkel.simons1@gmail.com>
2024-02-18 16:42:17 +0100

    changed file structure

--- LinkedList.js
+++ LinkedList.js
@@ -1,68 +1,75 @@
-export class LinkedList{
-    head;
-    tail;
+class LinkedList{
     constructor(head, tail) {
         this.head = head;
         this.tail = tail;
     }
 
-    constructor(){
-
+    dumpList() {
+        let a_node = this.head;
+        while (a_node != null) {
+            console.log(`
+            node: ${a_node.data}
+            --------------
+                prev: ${a_node.prev?.data}
+                next: ${a_node.next?.data}
+            `);
+            a_node = a_node.next;
+        } 
     }
 
     add(payload) {
-
-    }
-
-    addLast(payload) {
-
+        this.tail.next = payload;
+        payload.prev = this.tail;
+        this.tail = payload;
     }
 
     addFirst(payload) {
-
+        this.head.prev = payload;
+        payload.next = this.head;
+        this.head = payload;
     }
 
     clear() {
-
+        this.head = null;
+        this.tail = null;
     }
 
-    get(index) {
-
+    get(i) {
+        let j = 0;
+        let current = this.head;
+        while (i != j){
+            j++;
+            current = current.next;
+        }
+        return current;
     }
 
-    indexOf(i){
-
+    indexOf(payload) {
+        let i = 0;
+        let current = this.head;
+        while (current !== null && current.data !== payload) {
+            i++;
+            current = current.next;
+        }
+        if (current == null) {
+            return -1
+        }
+        return i;  
     }
 
     insertAfter(i, payload) {
-
+        payload.next = this.get(i).next;
+        payload.prev = this.get(i);
+        this.get(i).next = payload;
+        payload.next.prev = payload;
     }
 
     insertBefore(i, payload) {
-
-    }
-
-    first() {
-        return this.head;
-    }
-
-    last() {
-        return this.tail;
-    }
-
-    remove(i) {
-
-    }
-
-    removeFirst(){
-
-    }
-
-    removeLast() {
-
-    }
-
-    toString() {
-        return "Head: " + this.head + ", Tail: " + this.tail
+        payload.next = this.get(i);
+        payload.prev = this.get(i).prev;
+        this.get(i).prev.next = payload;
+        this.get(i).prev = payload;
     }
 }
+
+export default LinkedList;
--- run.js
+++ run.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import LinkedList from "./LinkedList.js";
+
 window.addEventListener('DOMContentLoaded', start);
 
 
@@ -25,80 +27,6 @@ node1.next = node2;
 node2.prev = node1;
 node2.next = node3;
 node3.prev = node2;
-
-class LinkedList{
-    constructor(head, tail) {
-        this.head = head;
-        this.tail = tail;
-    }
-
-    dumpList() {
-        let a_node = this.head;
-        while (a_node != null) {
-            console.log(`
-            node: ${a_node.data}
-            --------------
-                prev: ${a_node.prev?.data}
-                next: ${a_node.next?.data}
-            `);
-            a_node = a_node.next;
-        } 
-    }
-
-    add(payload) {
-        this.tail.next = payload;
-        payload.prev = this.tail;
-        this.tail = payload;
-    }
-
-    addFirst(payload) {
-        this.head.prev = payload;
-        payload.next = this.head;
-        this.head = payload;
-    }
-
-    clear() {
-        this.head = null;
-        this.tail = null;
-    }
-
-    get(i) {
-        let j = 0;
-        let current = this.head;
-        while (i != j){
-            j++;
-            current = current.next;
-        }
-        return current;
-    }
-
-    indexOf(payload) {
-        let i = 0;
-        let current = this.head;
-        while (current !== null && current.data !== payload) {
-            i++;
-            current = current.next;
-        }
-        if (current == null) {
-            return -1
-        }
-        return i;  
-    }
-
-    insertAfter(i, payload) {
-        payload.next = this.get(i).next;
-        payload.prev = this.get(i);
-        this.get(i).next = payload;
-        payload.next.prev = payload;
-    }
-
-    insertBefore(i, payload) {
-        payload.next = this.get(i);
-        payload.prev = this.get(i).prev;
-        this.get(i).prev.next = payload;
-        this.get(i).prev = payload;
-    }
-}
 
 function start() {
     console.log("JavaScript is running");
